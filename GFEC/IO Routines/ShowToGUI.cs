b3432f30411d8939a7893629a7094ce2608ec45c
Vasilis Merevis <vasilis.merevis@outlook.com>
2019-07-11 01:26:49 +0300

    Charts now use ObservablePoint with X and Y coordinates

--- GFEC/IO Routines/ShowToGUI.cs
+++ GFEC/IO Routines/ShowToGUI.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Defaults;
 using LiveCharts.Wpf;
 
 namespace GFEC
@@ -17,18 +19,24 @@ namespace GFEC
             int line = 0;
             double[] xAxis = new double[analysisResults.DynamicSolution.Count];
             double[] yAxis = new double[analysisResults.DynamicSolution.Count];
-            while (step < analysisResults.DynamicSolution.Count - 1)
+            var points = new ObservablePoint[analysisResults.DynamicSolution.Count];
+            while (step < analysisResults.DynamicSolution.Count)
             {
                 double[] sol = analysisResults.DynamicSolution[step];
                 xAxis[line] = analysisResults.TimeSteps[step];
                 yAxis[line] = sol[dofNumber];
+                points[line] = new ObservablePoint() { X = analysisResults.TimeSteps[step], Y = sol[dofNumber] };
                 line = line + 1;
                 step = step + intervals;
+                
                 //if (step >= solution.Count-1)
                 //{
                 //    break;
                 //}
             }
+            var mapper = Mappers.Xy<ObservablePoint>() //in this case value is of type <ObservablePoint>
+                .X(value => value.X) //use the X property as X
+                .Y(value => value.Y); //use the Y property as Y
             SeriesCollection graph = new SeriesCollection
             {
                 //new LineSeries
@@ -37,7 +45,7 @@ namespace GFEC
                 //},
                 new LineSeries
                 {
-                    Values = new ChartValues<double>(yAxis)
+                    Values = new ChartValues<ObservablePoint>(points)
                 }
             };
             return graph;
