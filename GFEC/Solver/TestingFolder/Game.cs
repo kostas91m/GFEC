7cc9b7c3582f18f293f3a32fdc5f6e26c48d7dd1
Vasilis Merevis <vasilis.merevis@outlook.com>
2019-09-25 00:39:12 +0300

    Minor changes related to Buffers

--- GFEC/Solver/TestingFolder/Game.cs
+++ GFEC/Solver/TestingFolder/Game.cs
@@ -5,12 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
 using OpenTK.Graphics;
+//using OpenTK.Graphics.ES10;
+using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 
 namespace GFEC
 {
     public class Game : GameWindow
     {
+        float[] vertices = {
+    -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+     0.5f, -0.5f, 0.0f, //Bottom-right vertex
+     0.0f,  0.5f, 0.0f  //Top vertex
+};
+
+        int VertexBufferObject;
+
         public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
@@ -22,6 +32,38 @@ namespace GFEC
                 Exit();
             }
             base.OnUpdateFrame(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            VertexBufferObject = GL.GenBuffer();
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+            base.OnUnload(e);
         }
     }
 }
