66ee785116a8a7ec7bcbe86cc2077821a23b919e
Vasilis Merevis <vasilis.merevis@outlook.com>
2019-11-11 01:20:21 +0200

    Fixes in ContactNtN2DTh element. Now it allows proper heat flow. Futher testing is needed.

--- GFEC/Elements/ContactNtN2DTh.cs
+++ GFEC/Elements/ContactNtN2DTh.cs
@@ -23,7 +23,7 @@ namespace GFEC
             ElementFreedomSignature[1] = new bool[] { true, false, false, false, false, false };
             ElementFreedomSignature[2] = new bool[] { true, false, false, false, false, false };
             DisplacementVector = new double[2];
-            properties.SectionArea = ContactArea;
+            ContactArea = properties.SectionArea;
         }
 
         private double CalculateConductivity()
